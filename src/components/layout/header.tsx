f82fbefbb06e8b1774b14e641d60fa78744d9ed5
Ishan Veer <ishanpveer@gmail.com>
2023-07-21 21:34:49 +0530

    header done

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,10 +13,10 @@ export const Header = () => {
             <p className={css.heroContentOverline}>new product</p>
             <h1 className={css.heroContentHeading}>XX99 Mark II Headphones</h1>
             <p className={css.heroContentPara}>
-              Experience natural, lifelike audio and exceptional build quality
+              Experience natural, life like audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
-            <Link href="/">See Product</Link>
+            <Link href="/" className={css.productLink}>See Product</Link>
           </div>
           <div className={css.heroImageContainer}>
             <Image src={heroImage} alt="hero" className={css.heroImage} />
