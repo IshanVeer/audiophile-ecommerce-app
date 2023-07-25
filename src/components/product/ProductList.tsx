ded3af8ec671e12c76e157cea296a6b0c11ac245
Ishan Veer <ishanpveer@gmail.com>
2023-07-25 23:20:13 +0530

    rearranged products in such a way that new products are always on the top

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,43 +1,52 @@
-'use client';
+"use client";
 
 import React from "react";
 import css from "./ProductList.module.css";
 import Image from "next/image";
 import headphoneImage from "../../assets/product-xx99-mark-one-headphones/desktop/image-product.jpg";
 import ProductLinkButtonPrimary from "../UI/ProductLinkButtonPrimary";
-import {useParams} from "next/navigation";
-
-
-
+import { useParams } from "next/navigation";
 
 const ProductList: React.FC<{ products: string[] }> = ({ products }) => {
-    const params = useParams();
-    console.log(params.categories, 'params');
-    console.log(products, 'products');
-    
- 
-    
+  const params = useParams();
+  console.log(params.categories, "params");
+  console.log(products, "products");
+
   return (
     <div>
       <div className={css.productListContainer}>
         <ul className={css.productList}>
-          {products.map((product) =>  product.category===params.categories && (
-            <li className={css.productListItem} key={product.id}>
-              <div className={css.productImageContainer}>
-                <Image className={css.productImage} src={headphoneImage} alt={product.name} />
-              </div>
-              <div className={css.productTextContainer}>
-                {product.new && <p className={css.productOverline}>New Product</p>}
-                <h2 className={css.productHeading}>{product.name}</h2>
-                <p className={css.productDescription}>{product.description}</p>
-                <ProductLinkButtonPrimary path="/" type="primary" name="See Product"/>
-              </div>
-              
-            </li>
-
-          ))}
+          {products
+            .sort((a, b) => (a.new === b.new ? 0 : a.new ? -1 : 1))
+            .map(
+              (product) =>
+                product.category === params.categories && (
+                  <li className={css.productListItem} key={product.id}>
+                    <div className={css.productImageContainer}>
+                      <Image
+                        className={css.productImage}
+                        src={headphoneImage}
+                        alt={product.name}
+                      />
+                    </div>
+                    <div className={css.productTextContainer}>
+                      {product.new && (
+                        <p className={css.productOverline}>New Product</p>
+                      )}
+                      <h2 className={css.productHeading}>{product.name}</h2>
+                      <p className={css.productDescription}>
+                        {product.description}
+                      </p>
+                      <ProductLinkButtonPrimary
+                        path="/"
+                        type="primary"
+                        name="See Product"
+                      />
+                    </div>
+                  </li>
+                )
+            )}
         </ul>
-        
       </div>
     </div>
   );
