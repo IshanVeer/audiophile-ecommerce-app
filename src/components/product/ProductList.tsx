edd62035d67af256f57105454e2d366f9d2b99cd
Ishan Veer <ishanpveer@gmail.com>
2023-07-25 23:12:59 +0530

    conditionally rendered products according to their category

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,20 +1,27 @@
+'use client';
+
 import React from "react";
 import css from "./ProductList.module.css";
 import Image from "next/image";
 import headphoneImage from "../../assets/product-xx99-mark-one-headphones/desktop/image-product.jpg";
 import ProductLinkButtonPrimary from "../UI/ProductLinkButtonPrimary";
+import {useParams} from "next/navigation";
 
 
 
 
 const ProductList: React.FC<{ products: string[] }> = ({ products }) => {
-  
+    const params = useParams();
+    console.log(params.categories, 'params');
+    console.log(products, 'products');
+    
+ 
     
   return (
     <div>
       <div className={css.productListContainer}>
         <ul className={css.productList}>
-          {products.map((product) =>  (
+          {products.map((product) =>  product.category===params.categories && (
             <li className={css.productListItem} key={product.id}>
               <div className={css.productImageContainer}>
                 <Image className={css.productImage} src={headphoneImage} alt={product.name} />
@@ -25,9 +32,12 @@ const ProductList: React.FC<{ products: string[] }> = ({ products }) => {
                 <p className={css.productDescription}>{product.description}</p>
                 <ProductLinkButtonPrimary path="/" type="primary" name="See Product"/>
               </div>
+              
             </li>
+
           ))}
         </ul>
+        
       </div>
     </div>
   );
