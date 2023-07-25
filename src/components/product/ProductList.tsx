980a735915f02d718af262a4116fd48e02546e78
Ishan Veer <ishanpveer@gmail.com>
2023-07-25 21:06:38 +0530

    product list rendered from the json data file

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,22 +1,36 @@
-
-import React from 'react';
+import React from "react";
 import css from "./ProductList.module.css";
+import Image from "next/image";
+import headphoneImage from "../../assets/product-xx99-mark-one-headphones/desktop/image-product.jpg";
+import ProductLinkButtonPrimary from "../UI/ProductLinkButtonPrimary";
 
 
-const ProductList:React.FC< {products:string[]} > = ({products}) => {
 
-    const productList = products.map((product:any) => <li key={product.id}>{product.name}</li>)
+
+const ProductList: React.FC<{ products: string[] }> = ({ products }) => {
+  
+    
   return (
     <div>
-        <div className={css.productListContainer}>
-            <ul>
-                {productList}
-            </ul>
-            
-
-        </div>
+      <div className={css.productListContainer}>
+        <ul className={css.productList}>
+          {products.map((product) =>  (
+            <li className={css.productListItem} key={product.id}>
+              <div className={css.productImageContainer}>
+                <Image className={css.productImage} src={headphoneImage} alt={product.name} />
+              </div>
+              <div className={css.productTextContainer}>
+                {product.new && <p className={css.productOverline}>New Product</p>}
+                <h2 className={css.productHeading}>{product.name}</h2>
+                <p className={css.productDescription}>{product.description}</p>
+                <ProductLinkButtonPrimary path="/" type="primary" name="See Product"/>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
