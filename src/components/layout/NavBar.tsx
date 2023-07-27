cf9bb992b9933e978d1966392092d8896ede4865
Ishan Veer <ishanpveer@gmail.com>
2023-07-27 21:57:32 +0530

    made navbar dynamic, now if there is a new category in the json data itwill be directly added to the nav bar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,49 +1,65 @@
-'use client'
+"use client";
 import React from "react";
 import css from "./NavBar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../assets/shared/desktop/logo.svg";
-import  NavCartButton  from "./NavCartButton";
-import {usePathname} from "next/navigation";
+import NavCartButton from "./NavCartButton";
+import { usePathname } from "next/navigation";
 
+interface productProps {
+  _id: number;
+  category: string;
+}
 
-const NavBar = () => {
+const NavBar: React.FC<{ products: productProps[] }> = ({ products }) => {
   const currentPathname = usePathname();
-  
-  
 
-  
+  // so that there is no repetetion of categories in the navbar
+  // products.map((product) => product.category) returns an array of categories
+  // new Set() returns an object with unique categories
+  // Array.from() turns that object into an array again
+  const uniqueCategories = Array.from(
+    new Set(products.map((product) => product.category))
+  );
+
   return (
     <div>
       <div className={css.navBarContainer}>
         <nav className={css.navigation}>
-          <Link  href="/">
+          <Link href="/">
             {/* logo */}
             <Image src={logo} alt="logo" className={css.logo} />
           </Link>
           {/* category links */}
           <ul className={css.navList}>
             <li className={css.navListItem}>
-              <Link className={currentPathname === '/' ? css.navListItemLinkActive : css.navListItemLink}  href="/">
+              <Link
+                className={
+                  currentPathname === "/"
+                    ? css.navListItemLinkActive
+                    : css.navListItemLink
+                }
+                href="/"
+              >
                 HOME
               </Link>
             </li>
-            <li className={css.navListItem}>
-              <Link className={currentPathname === '/headphones' ? css.navListItemLinkActive : css.navListItemLink} href="/headphones">
-                HEADPHONES
-              </Link>
-            </li>
-            <li className={css.navListItem}>
-              <Link className={currentPathname === '/earphones' ? css.navListItemLinkActive : css.navListItemLink} href="/earphones">
-                EARPHONES
-              </Link>
-            </li>
-            <li className={css.navListItem}>
-              <Link className={currentPathname === '/speakers' ? css.navListItemLinkActive : css.navListItemLink} href="/speakers">
-                SPEAKERS
-              </Link>
-            </li>
+
+            {uniqueCategories.map((category) => (
+              <li className={css.navListItem} key={category}>
+                <Link
+                  className={
+                    currentPathname === `/${category}`
+                      ? css.navListItemLinkActive
+                      : css.navListItemLink
+                  }
+                  href={`/${category}`}
+                >
+                  {category}
+                </Link>
+              </li>
+            ))}
           </ul>
           <NavCartButton />
         </nav>
