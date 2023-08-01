47988448f4de56bdfe26565a328c6cf2cf299122
Ishan Veer <ishanpveer@gmail.com>
2023-08-01 14:19:47 +0530

    added individual product data to productDetail page

--- src/components/product/ProductDetail.tsx
+++ src/components/product/ProductDetail.tsx
@@ -1,18 +1,21 @@
+"use client";
 import React from "react";
 import css from "./ProductDetail.module.css";
 import { ProductProps } from "@/types/types";
+import { useParams } from "next/navigation";
 
 const ProductDetail: React.FC<{ products: ProductProps[] }> = ({
   products,
 }) => {
-   console.log(products, 'products');
+   const params = useParams();
+    const product = products.find((product) => product.slug === params.slug);
   return (
     <div>
       <div className={css.productDetailContainer}>
         {/* image + brief info */}
         <div className={css.productMainLayout}>
           <div className={css.productImageContainer}></div>
-          <div className={css.productActionConatiner}></div>
+          <div className={css.productActionConatiner}><h1>{product.name}</h1></div>
         </div>
         {/* features + box info */}
         <div className={css.productFeaturesLayout}>
