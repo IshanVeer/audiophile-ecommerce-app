26853b1a4a0e45824a64cc21f48a11b6ce5f386d
Ishan Veer <ishanpveer@gmail.com>
2025-07-11 14:54:59 +0530

    created mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 import {
   Drawer,
   DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { categoryData } from "@/constants";
+import Link from "next/link";
 
 const MobileNav = () => {
   return (
     <div className="md:hidden px-12 py-8 bg-dark-100 flex justify-between items-center">
       <div className="flex items-center gap-6">
         <Drawer direction="top">
-          <DrawerTrigger asChild>
+          <DrawerTrigger>
             <Image
               src="/assets/shared/tablet/icon-hamburger.svg"
               alt="nav-menu"
@@ -23,13 +23,42 @@ const MobileNav = () => {
               className="object-cover"
             />
           </DrawerTrigger>
-          <DrawerContent>
-            <DrawerHeader>
-              <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-              <DrawerDescription>
-                This action cannot be undone.
-              </DrawerDescription>
-            </DrawerHeader>
+          <DrawerContent className="px-12 pt-32 pb-26">
+            <DrawerTitle className="hidden">
+              Are you absolutely sure?
+            </DrawerTitle>
+            <ul className="flex flex-wrap gap-x-3 mx-auto justify-center items-center">
+              {categoryData.map((category) => (
+                <li
+                  className="bg-light-200 flex items-end py-7 relative h-[180px] w-[214px] rounded-md"
+                  key={category.category}
+                >
+                  <Image
+                    src={category.categoryImage}
+                    alt={category.category}
+                    height={700}
+                    width={700}
+                    className="w-[180px] absolute -top-14 left-5 object-contain"
+                  />
+                  <div className="w-36 text-center mx-auto">
+                    <h6 className="h6-bold py-3">{category.category}</h6>
+                    <Link
+                      href={`/${category.category}`}
+                      className="flex items-center gap-2 text-[13px] font-bold hover:text-primary-500 leading-[18px] uppercase tracking-[1px] justify-center text-dark-100/50 transition duration-150"
+                    >
+                      <p>Shop</p>
+
+                      <Image
+                        src="/assets/shared/desktop/icon-arrow-right.svg"
+                        alt="link"
+                        height={7}
+                        width={7}
+                      />
+                    </Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </DrawerContent>
         </Drawer>
 
