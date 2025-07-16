e9a88c489ea4c5341dbf4d4821458a2b2edabff0
Ishan Veer <ishanpveer@gmail.com>
2025-07-16 21:57:53 +0530

    setup cart state

--- context/CartProvider.tsx
+++ context/CartProvider.tsx
@@ -1,9 +1,20 @@
 "use client";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const CartContext = createContext(undefined);
 
 const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  // initialise cart state
+  const [cart, setCart] = useState([]);
+
+  // add to cart
+
+  // increase cart item
+
+  // decrease cart item and if 1 then remove it
+
+  // remove all cart items
+
   const value = {};
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
