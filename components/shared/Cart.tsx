1459aa838f788d111ba2c0b53a003a4e30688ea3
Ishan Veer <ishanpveer@gmail.com>
2025-07-19 21:10:28 +0530

    added empty cart page

--- components/shared/Cart.tsx
+++ components/shared/Cart.tsx
@@ -10,6 +10,7 @@ import {
 import { useCartContext } from "@/context/CartProvider";
 import Button from "../ui/Button";
 import { shortenName } from "@/lib/utils";
+import EmptyCart from "./EmptyCart";
 
 const Cart = () => {
   const { cart, increaseCartItem, decreaseCartItem, removeAllItems } =
@@ -35,63 +36,69 @@ const Cart = () => {
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className=" absolute top-25 left-6 sm:left-90 sm:top-25 lg:-left-90 lg:top-20   w-[377px] px-7 py-9">
-        <div className="flex justify-between">
-          <h5 className="h6-bold">Cart ({cart.length}) </h5>
-          <button
-            onClick={removeAllItems}
-            className="text-dark-100/50 body border-b cursor-pointer hover:text-dark-100/55 transition duration-150"
-          >
-            Remove All
-          </button>
-        </div>
-        <ul className="pt-10">
-          {cart.map((item) => (
-            <li
-              key={item.id}
-              className="flex justify-between pb-8 items-center gap-4"
-            >
-              <div className="flex w-2/3 items-center gap-4">
-                <Image
-                  src={item.image.desktop}
-                  alt={item.name}
-                  width={100}
-                  height={100}
-                  className="w-1/3 rounded-md"
-                />
-
-                <div className="w-1/2">
-                  <p className="font-bold text-[15px] uppercase">
-                    {shortenName(item.name)}
-                  </p>
-                  <p className="text-dark-100/50 text-[14px] font-bold">
-                    {`$${item.price}`}
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex w-[96px] py-1 justify-around text-dark-100/25 bg-light-300  items-center">
-                <button
-                  onClick={() => decreaseCartItem(item.id)}
-                  className="cursor-pointer"
+        {cart.length > 0 ? (
+          <div>
+            <div className="flex justify-between">
+              <h5 className="h6-bold">Cart ({cart.length}) </h5>
+              <button
+                onClick={removeAllItems}
+                className="text-dark-100/50 body border-b cursor-pointer hover:text-dark-100/55 transition duration-150"
+              >
+                Remove All
+              </button>
+            </div>
+            <ul className="pt-10">
+              {cart.map((item) => (
+                <li
+                  key={item.id}
+                  className="flex justify-between pb-8 items-center gap-4"
                 >
-                  -
-                </button>
-                <p className="text-dark-100 subtitle">{item.quantity}</p>
-                <button
-                  onClick={() => increaseCartItem(item.id)}
-                  className="cursor-pointer"
-                >
-                  +
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="flex justify-between items-center py-8">
-          <p className="body uppercase text-dark-100/50">Total</p>
-          <p className="h6-bold">{`$${totalPrice}`}</p>
-        </div>
-        <Button className="w-full" label="checkout" isLink route="/" />
+                  <div className="flex w-2/3 items-center gap-4">
+                    <Image
+                      src={item.image.desktop}
+                      alt={item.name}
+                      width={100}
+                      height={100}
+                      className="w-1/3 rounded-md"
+                    />
+
+                    <div className="w-1/2">
+                      <p className="font-bold text-[15px] uppercase">
+                        {shortenName(item.name)}
+                      </p>
+                      <p className="text-dark-100/50 text-[14px] font-bold">
+                        {`$${item.price}`}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex w-[96px] py-1 justify-around text-dark-100/25 bg-light-300  items-center">
+                    <button
+                      onClick={() => decreaseCartItem(item.id)}
+                      className="cursor-pointer"
+                    >
+                      -
+                    </button>
+                    <p className="text-dark-100 subtitle">{item.quantity}</p>
+                    <button
+                      onClick={() => increaseCartItem(item.id)}
+                      className="cursor-pointer"
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-between items-center py-8">
+              <p className="body uppercase text-dark-100/50">Total</p>
+              <p className="h6-bold">{`$${totalPrice}`}</p>
+            </div>
+            <Button className="w-full" label="checkout" isLink route="/" />
+          </div>
+        ) : (
+          <EmptyCart />
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
